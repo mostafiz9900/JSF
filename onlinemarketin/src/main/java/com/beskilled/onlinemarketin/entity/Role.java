bf1da843957db0b26a6f34518e9d92850da4d26e
mostafiz9900 <mostafiz9900@gmail.com>
2018-12-20 14:10:50 +0600

    my project

---
+++ onlinemarketin/src/main/java/com/beskilled/onlinemarketin/entity/Role.java
@@ -0,0 +1,29 @@
+package com.beskilled.onlinemarketin.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "user_id", unique =true, nullable = false)
+    private String userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
---
+++ onlinemarketin/src/test/java/com/beskilled/onlinemarketin/OnlinemarketinApplicationTests.java
@@ -0,0 +1,17 @@
+package com.beskilled.onlinemarketin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlinemarketinApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
